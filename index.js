7bbdfe7834eb5a14e779896fe4a26573127c7a2c
Bill Brown <bill.a.brown90@gmail.com>
2023-02-23 16:28:06 -0800

    Completed DOM lab

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const main = document.getElementById("main");
 main.remove();
 
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent= "YOUR-NAME is the champion";
